8f0b10dfc9eda316c1a0f493404a2e173841472c
Vaisakhkm2625 <vaisakhkm2625@gmail.com>
2021-07-03 21:53:20 +0530

    using state variables for js and css

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  style ={
+
+    fontSize: '10px',
+    fontWeight: "bold"
+  }
+
+
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
-        <button>increment</button>
+        <span style={this.style} className="badge badge-primary m-2">{this.formatCount()}</span>
+
+        <button style={{fontSize: "20px"}} className="btn btn-secondary btn-sm">increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 }
 
