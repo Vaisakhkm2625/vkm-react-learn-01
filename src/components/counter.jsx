200dcccc66615be2ff15f64cb96925eb22b9c017
Vaisakhkm2625 <vaisakhkm2625@gmail.com>
2021-07-03 22:17:57 +0530

    rendering lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
   style = {
@@ -23,6 +24,12 @@ class Counter extends Component {
         >
           increment
         </button>
+
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
